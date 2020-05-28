f42d7e9d39022e80e5cfb3e3ddc4cfbe1d7904cd
maciejjablonsky <maciekjablonsky@gmail.com>
2020-05-29 00:29:59 +0200

    Plotting in loop

--- plotting.py
+++ plotting.py
@@ -4,63 +4,28 @@ import matplotlib.pyplot as plt
 from splains import cubic_splains
 from lagrange import lagrange
 
-
-
 plots_path = 'plots/'
-
-filename = 'chelm'
-
+filename = 'Obiadek'
+method = 'lagrange'
 data = pd.read_csv('data/' + filename +'.csv')
 
-steps = [150, 100, 50, 30]
-fig, ax = plt.subplots(2,2)
+steps = [120, 85, 50, 35]
+nrows, ncols = 2,2
+fig = plt.figure()
 fig.suptitle('Interpolacja metodą Lagrange\'a')
 
-X = np.array(data['distance'])[::steps[0]]
-Y = np.array(data['height'])[::steps[0]]
-x = np.linspace(min(X), max(X), 1000)
-y = lagrange(x, X, Y)
-ax[0,0].plot(X, Y, '.', label='Punkty pomiarowe')
-ax[0,0].plot(x, y, label='Krzywa po interpolacji')
-ax[0,0].legend()
-ax[0,0].set_ylabel('Wysokość [m]')
-ax[0,0].set_xlabel('Dystans [m]')
-ax[0,0].title.set_text('Interpolacja przy %d węzłach' % (len(X)))
-
-X = np.array(data['distance'])[::steps[1]]
-Y = np.array(data['height'])[::steps[1]]
-x = np.linspace(min(X), max(X), 1000)
-y = lagrange(x, X, Y)
-ax[0,1].plot(X, Y, '.', label='Punkty pomiarowe')
-ax[0,1].plot(x,y, label='Krzywa po interpolacji')
-ax[0,1].legend()
-ax[0,1].set_ylabel('Wysokość [m]')
-ax[0,1].set_xlabel('Dystans [m]')
-ax[0,1].title.set_text('Interpolacja przy %d węzłach' % len(X))
-
-X = np.array(data['distance'])[::steps[2]]
-Y = np.array(data['height'])[::steps[2]]
-x = np.linspace(min(X), max(X), 1000)
-y = lagrange(x, X, Y)
-ax[1,0].plot(X, Y, '.', label='Punkty pomiarowe')
-ax[1,0].plot(x,y, label='Krzywa po interpolacji')
-ax[1,0].legend()
-ax[1,0].set_ylabel('Wysokość [m]')
-ax[1,0].set_xlabel('Dystans [m]')
-ax[1,0].title.set_text('Interpolacja przy %d węzłach' % len(X))
-
-X = np.array(data['distance'])[::steps[3]]
-Y = np.array(data['height'])[::steps[3]]
-x = np.linspace(min(X), max(X), 1000)
-y = lagrange(x, X, Y)
-ax[1,1].plot(X, Y, '.', label='Punkty pomiarowe')
-ax[1,1].plot(x,y, label='Krzywa po interpolacji')
-ax[1,1].legend()
-ax[1,1].set_ylabel('Wysokość [m]')
-ax[1,1].set_xlabel('Dystans [m]')
-ax[1,1].title.set_text('Interpolacja przy %d węzłach' % len(X))
-
-
+for i, step in enumerate(steps):
+    X = np.array(data['distance'])[::step]
+    Y = np.array(data['height'])[::step]
+    x = np.linspace(min(X), max(X), 1000)
+    y = lagrange(x, X, Y)
+    ax = fig.add_subplot(nrows, ncols, i +1) 
+    ax.plot(X, Y, '.', label='Punkty pomiarowe')
+    ax.plot(x,y, label='Krzywa po interpolacji')
+    ax.set_ylabel('Wysokość [m]')
+    ax.set_xlabel('Dystans [m]')
+    ax.title.set_text('Interpolacja przy %d węzłach' % len(X))
+    ax.legend()
 plt.tight_layout()
 plt.show()
-fig.savefig(plots_path + filename + '.png', dpi=200) 
+fig.savefig(plots_path + method + '_' + filename + '.png', dpi=200) 
