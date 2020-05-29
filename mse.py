b9b28dd252454a45bfd775d57b086472bb484f4c
maciejjablonsky <maciekjablonsky@gmail.com>
2020-05-29 12:54:44 +0200

    Added plots of lagrange, splains and mean square error

---
+++ mse.py
@@ -0,0 +1,43 @@
+import numpy as np
+from lagrange import lagrange
+from splains import cubic_splains
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def mse(expected, evaluated): return (np.square(expected - evaluated)).mean()
+
+plots_path = 'plots/'
+method_name = 'splains'
+
+
+method = {
+    'lagrange':lagrange,
+    'splains':cubic_splains
+}
+
+files = ['chelm', 'MountEverest', 'SpacerniakGdansk', 'WielkiKanionKolorado', 'Redlujjj']
+
+for filename in files:
+    print('Computing %s %s error'%(filename, method_name))
+    max_nodes = 200
+    nrows, ncols = 2,2
+    data = pd.read_csv('data/' + filename +'.csv')
+    err = []
+    for nodes in range(2, max_nodes, 1):
+        print('\rnodes: %d'% nodes, end='')
+        x_real = np.array(data['distance'])
+        y_real = np.array(data['height'])
+        idx = np.round(np.linspace(0, len(data) - 1, nodes).astype(int))
+        X = x_real[idx]
+        Y = y_real[idx]
+        _, y_interpolated = method[method_name](x_real, X, Y)
+        err.append(mse(y_real, y_interpolated))
+    print('Plotting %s %s error' % (filename, method_name))
+    fig = plt.figure()
+    fig.suptitle('Średni błąd kwadratowy metody funkcji sklejanych\'a')
+    plt.semilogy(range(2, max_nodes), err)
+    plt.grid()
+    plt.ylabel('Wartość błędu [m]')
+    plt.xlabel('Ilość węzłów [n]')
+    fig.savefig(plots_path + 'error_' + method_name + ' ' + filename + '.png', dpi=200)
+plt.show()
--- plotting.py
+++ plotting.py
@@ -6,26 +6,42 @@ from lagrange import lagrange
 
 plots_path = 'plots/'
 filename = 'Obiadek'
-method = 'lagrange'
-data = pd.read_csv('data/' + filename +'.csv')
+method_name = 'lagrange'
 
-steps = [120, 85, 50, 35]
-nrows, ncols = 2,2
-fig = plt.figure()
-fig.suptitle('Interpolacja metodą Lagrange\'a')
 
-for i, step in enumerate(steps):
-    X = np.array(data['distance'])[::step]
-    Y = np.array(data['height'])[::step]
-    x = np.linspace(min(X), max(X), 1000)
-    y = lagrange(x, X, Y)
-    ax = fig.add_subplot(nrows, ncols, i +1) 
-    ax.plot(X, Y, '.', label='Punkty pomiarowe')
-    ax.plot(x,y, label='Krzywa po interpolacji')
-    ax.set_ylabel('Wysokość [m]')
-    ax.set_xlabel('Dystans [m]')
-    ax.title.set_text('Interpolacja przy %d węzłach' % len(X))
-    ax.legend()
-plt.tight_layout()
-plt.show()
-fig.savefig(plots_path + method + '_' + filename + '.png', dpi=200) 
+method = {
+    'lagrange':lagrange,
+    'splains':cubic_splains
+}
+
+files = ['chelm', 'MountEverest', 'SpacerniakGdansk', 'WielkiKanionKolorado', 'Redlujjj']
+
+for filename in files:
+    nodes_number = {
+        'lagrange':[3,5, 10, 50],
+        'splains': [3, 5, 10, 50]
+        }
+    nrows, ncols = 2,2
+    fig = plt.figure(figsize=(15, 8), dpi=300)
+    fig.suptitle('Interpolacja metodą Lagrange\'a. Zbiór danych %s' % filename)
+    data = pd.read_csv('data/' + filename +'.csv')
+    print('Computing %s %s' % (filename, method_name))
+    plt.subplots_adjust(hspace=0.5)
+    for i, nodes in enumerate(nodes_number[method_name]):
+        idx = np.round(np.linspace(0, len(data) - 1, nodes).astype(int))
+        X = np.array(data['distance'])[idx]
+        Y = np.array(data['height'])[idx]
+        x = np.linspace(min(X), max(X), 1000)
+        x, y = method[method_name](x, X, Y)
+        ax = fig.add_subplot(nrows, ncols, i +1)
+        ax.grid()
+        ax.plot(X, Y, 'o', label='Węzły podane do interpolacji')
+        ax.plot(np.array(data['distance']), np.array(data['height']), label='Wartości rzeczywiste')
+        ax.plot(x,y, label='Krzywa po interpolacji')
+        ax.set_ylabel('Wysokość [m]')
+        ax.set_xlabel('Dystans [m]')
+        ax.title.set_text('Interpolacja przy %d węzłach' % len(X))
+        ax.legend()
+    print('Plotting %s %s' % (filename, method_name))
+    fig.tight_layout(rect=[0, 0, 1, 0.95])    
+    fig.savefig(plots_path + method_name + '_' + filename + '.png', bbox='tight') 
